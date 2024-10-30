65be7a197fda4dadcfbcc4a9bf3b8efff2de7ee6
VitorTz <vitor.fsz@proton.me>
2024-10-30 09:37:55 -0300

    Update

--- ExactChange.cpp
+++ ExactChange.cpp
@@ -1,49 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <functional>
 
 
-static const int MAX_PRICE = 10000;
+constexpr int MAX_PRICE = 10000;
+
 
 int main() {  
-    int T;
-    std::cin >> T;
+    int nCases;
+    std::cin >> nCases;
 
-    while (T--) {
+    while (nCases--) {
         int p, n;
         std::cin >> p >> n;
         std::vector<int> coins(n);
-        
+
         for (int i = 0; i < n; ++i) {
             std::cin >> coins[i];
         }
-    
-        std::vector<int> dp(MAX_PRICE + 1, 0);
+        
+        std::vector<int> table(MAX_PRICE + 1, 0);
 
         for (int i = 0; i < n; ++i) {
             for (int j = MAX_PRICE; j >= 0; --j) {
-                if (dp[j]  && j + coins[i] <= MAX_PRICE) {
-                    if (dp[j + coins[i]] == 0) {
-                        dp[j + coins[i]] = dp[j] + 1;
+                if (table[j] && j + coins[i] <= MAX_PRICE) {
+                    if (table[j + coins[i]] == 0) {
+                        table[j + coins[i]] = table[j] + 1;
                     } else {
-                        dp[j + coins[i]] = std::min(dp[j + coins[i]], dp[j] + 1);
+                        table[j + coins[i]] = std::min(table[j + coins[i]], table[j] + 1);
                     }                    
                 }
             }
-            
-            if (dp[coins[i]] == 0) {
-                dp[coins[i]] = 1;
+            if (table[coins[i]] == 0) {
+                table[coins[i]] = 1;
             }
         }
 
         std::vector<int>::iterator iter(
-            find_if(dp.begin() + p, 
-            dp.end(),
-            std::bind(std::greater<int>(), 0))
+            std::find_if(
+                table.begin() + p,
+                table.end(),
+                [](const int x) { return x > 0; }                
+            )
         );
-
-        std::cout << iter - dp.begin() << " " << *iter << '\n';
+        std::cout << iter - table.begin() << " " << *iter << '\n';
     }
     return 0;
 }
